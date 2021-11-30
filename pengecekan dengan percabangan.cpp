83fffbb4247fcf9252d5c3591d8790819d3cea04
Rivaldo <92324602+Rivaldo2117051054@users.noreply.github.com>
2021-11-30 19:45:53 +0700

    Add files via upload

---
+++ pengecekan dengan percabangan.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+// mengecek syarat untuk sekolah artis minimal tinggi lebih 170, berat lebih 55 dan usia 15
+int main() {
+    int tinggi,berat,usia;
+    //memasukan angka sesuai
+    cin>>tinggi;
+    cin>>berat;
+    cin>>usia;
+if(tinggi>170 && berat>55 && usia>=15){
+	//jika semua benar maka akan lolos
+        cout<<"Lolos"<<endl;
+    }
+    else{
+    	// akan gagal jika salah satu tidak lolos
+        cout<<"Tidak Lolos"<<endl;
+    }  
+    return 0;
+}
